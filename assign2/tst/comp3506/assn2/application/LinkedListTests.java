bbeab38a4b6088fb3106b4b071e8f35b709e9437
acarrick <anthonix@gmail.com>
2018-10-02 21:51:46 +1000

    added LinkedList tests

--- assign2/tst/comp3506/assn2/application/LinkedListTests.java
+++ assign2/tst/comp3506/assn2/application/LinkedListTests.java
@@ -9,9 +9,9 @@ public class LinkedListTests {
 	@Test
 	public void testAddTwo() {
 		LinkedList<Object> ll = new LinkedList<Object>();
-		ll.append(new Integer(1));
+		ll.add(new Integer(1));
 		
-		ll.append(new Integer(2));
+		ll.add(new Integer(2));
 		Integer expected = 2;
 		Integer actual = ll.size;
 		assertEquals(expected, actual);
@@ -19,5 +19,22 @@ public class LinkedListTests {
 		assertEquals(1, ll.getHead().element);
 		assertEquals(2, ll.getHead().next.element);
 	}
-
+	@Test
+	public void loop() {
+		LinkedList<Object> ll = new LinkedList<Object>();
+		ll.add(new Integer(1));
+		
+		ll.add(new Integer(2));
+		ll.add(new Integer(3));
+		ll.add(new Integer(4));
+		
+		assertEquals(4, ll.size);
+		Integer currentCount = 1;
+		while (ll.hasNext()) {
+			Object current = ll.next().getElement();
+			System.out.println(current);
+			assertEquals(currentCount, current);
+			currentCount++;
+		}
+	}
 }
